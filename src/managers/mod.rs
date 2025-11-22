c343c8f7d96f63485054401346c3e0b66e975c08
choithuti <141320770+choithuti@users.noreply.github.com>
2025-11-22 10:03:43 +0700

    Update mod.rs

--- src/managers/mod.rs
+++ src/managers/mod.rs
@@ -1,18 +1,14 @@
-// src/managers/mod.rs
+pub mod p2p_manager;
 pub mod transaction_manager;
 pub mod consensus_engine;
-pub mod p2p_manager;
 pub mod security_manager;
 pub mod storage_manager;
 
 use crate::{snn_core::SNNCore, bus::MessageBus, crypto::CryptoEngine};
 use std::sync::Arc;
+use tracing::info;
 
-pub fn start_all(snn: Arc<SNNCore>, bus: Arc<MessageBus>, crypto: Arc<CryptoEngine>) {
-    transaction_manager::spawn(snn.clone(), bus.clone());
-    consensus_engine::spawn(snn.clone(), bus.clone());
-    p2p_manager::spawn(snn.clone(), bus.clone(), crypto.clone());
-    security_manager::spawn(snn.clone(), bus.clone());
-    storage_manager::spawn(snn.clone(), bus.clone(), crypto);
-    tracing::info!("All PappapAIChain managers started – Ready for Testnet 2026");
+pub async fn start_all(_snn: Arc<SNNCore>, _bus: Arc<MessageBus>, _crypto: Arc<CryptoEngine>) {
+    info!("All PappapAIChain managers started – Ready for Testnet 2026");
+    p2p_manager::start_p2p().await;
 }
