b79417a6a5d75c08f4f218ea1a6cdec632eded6f
choithuti <141320770+choithuti@users.noreply.github.com>
2025-11-22 09:43:21 +0700

    Update crypto.rs

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,4 +1,4 @@
-use aes_gcm::{Aes256Gcm, Key, Nonce, KeyInit, aead::{Aead, AeadCore}};
+use aes_gcm::{Aes256Gcm, Key, Nonce, KeyInit, aead::Aead};
 use rand::RngCore;
 
 pub struct CryptoEngine {
@@ -7,19 +7,19 @@ pub struct CryptoEngine {
 
 impl CryptoEngine {
     pub fn new(key: &[u8; 32]) -> Self {
-        Self { cipher: Aes256Gcm::new(Key::<Aes256Gcm>::from_slice(key)) }
+        Self { cipher: Aes256Gcm::new(Key::from_slice(key)) }
     }
 
     pub fn encrypt(&self, data: &[u8]) -> Vec<u8> {
         let mut nonce = [0u8; 12];
         rand::thread_rng().fill_bytes(&mut nonce);
-        let ciphertext = self.cipher.encrypt(Nonce::from_slice(&nonce), data).expect("encrypt fail");
-        [nonce.as_slice(), &ciphertext].concat()
+        let ciphertext = self.cipher.encrypt(Nonce::from_slice(&nonce), data).unwrap();
+        [nonce.to_vec(), ciphertext].concat()
     }
 
-    pub fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>, aes_gcm::Error> {
-        if data.len() < 12 { return Err(aes_gcm::Error); }
+    pub fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>, ()> {
+        if data.len() < 12 { return Err(()); }
         let (nonce, ciphertext) = data.split_at(12);
-        self.cipher.decrypt(Nonce::from_slice(nonce), ciphertext)
+        self.cipher.decrypt(Nonce::from_slice(nonce), ciphertext).map_err(|_| ())
     }
 }
